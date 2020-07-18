d0711248af3ddeb8a8c805d4c24104638edc9517
stefanmills <stefanmills8@gmail.com>
2020-07-18 02:03:09 +0100

    Automated Browser.py

---
+++ chromeauto.py
@@ -0,0 +1,13 @@
+import webbrowser as wb
+
+def chromeauto():
+    chrome_path="/opt/google/chrome/google-chrome"
+    websites={"twitter.com",
+              "gmail.com",
+              "coursera.org",
+              "udemy.com"}
+    for url in websites:
+        print("Opening "+url)
+        wb.get(chrome_path).open(url)
+
+chromeauto()
