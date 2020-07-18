e6e05c3ed83d58c121af0457d5a73ee6e10d1e20
stefanmills <stefanmills8@gmail.com>
2020-07-18 08:07:08 +0100

    piglatin code

--- piglatin.py
+++ piglatin.py
@@ -2,24 +2,32 @@
 
 #declaration of variable
 vowel=("a","e","i","o","u")
+consonant=("b","c","d","f","g","h","j","k","l",
+           "m","n","p","q","r","s","t","v","w","x","y","z")
 
 print("WELCOME TO MY PIGLATIN PROGRAM.")
 print("="*30)
 
 theword=input("Please input a word\n")
+theword=theword.lower()     #converts the word to lower case.
 
 def piglatin(theword):
+    store=""
+
+    #vowel conversion
     if theword.startswith(vowel):
-        word= theword+"way"
+        word=theword+"way"
         print("The piglatin of \""+theword+"\" is "+ word)
 
-    elif theword[0] and theword[1] not in vowel and str:
-        word=theword[2:len(theword)]+theword[0]+theword[1]+"ay"
-        print("The piglatin of \" "+theword+"\" is "+ word)
-    elif theword[0]not in vowel and theword[1] in vowel and str:
-        word=theword[1:len(theword)]+theword[0]+"ay"
-        print("The piglatin of \" "+theword+"\" is "+ word)
-    else:
-        print("Please start your word with a letter")
+    #consonant conversion
+    elif theword.startswith(consonant):
+        for i in range(0,len(theword)):
+            if theword[i] in consonant:
+                store= store+theword[i]
+            else:
+                word=theword[i:len(theword)]+store+"ay"
+                print("The piglatin of \""+theword+"\" is "+ word)
+                break
 
 piglatin(theword)
+
